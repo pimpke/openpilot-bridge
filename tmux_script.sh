ac26441372db35903526b3361220aa6cd59e8f96
Dimitrije Dimic <dimke92@gmail.com>
2023-04-12 16:39:06 +0200

    Clean up tmux_script.sh

--- tmux_script.sh
+++ tmux_script.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-tmux new -d -s carla-sim
+tmux new -d -s default
 if [[ "${SHOULD_RUN_OPENPILOT_SIM}" == "1" ]]; then
   tmux send-keys "cd /openpilot/tools/sim && ./launch_openpilot.sh" ENTER
   tmux neww
   tmux send-keys "cd /openpilot/tools/sim && ./bridge.py $*" ENTER
   tmux neww
 fi
-tmux send-keys "set -x" ENTER
-tmux send-keys "python --version" ENTER
-tmux send-keys 'echo $PYTHONPATH' ENTER
 tmux send-keys 'PYTHONPATH=/openpilot:/openpilot_bridge python -u main.py' ENTER
-tmux a -t carla-sim
+tmux a -t default
