ab112cb9feaee1367849c855953e6efd3b370d34
Dimitrije Dimic <dimke92@gmail.com>
2023-04-13 19:29:15 +0200

    Change openpilot_bridge to openpilot-bridge in PYTHONPATH in tmux_script.sh

--- tmux_script.sh
+++ tmux_script.sh
@@ -6,5 +6,5 @@ if [[ "${SHOULD_RUN_OPENPILOT_SIM}" == "1" ]]; then
   tmux send-keys "cd /openpilot/tools/sim && ./bridge.py $*" ENTER
   tmux neww
 fi
-tmux send-keys 'PYTHONPATH=/openpilot:/openpilot_bridge python -u main.py' ENTER
+tmux send-keys 'PYTHONPATH=/openpilot:/openpilot-bridge python -u main.py' ENTER
 tmux a -t default
