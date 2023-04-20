d298d095c859114bc340e24b06bdaae392bafbec
Dimitrije Dimic <dimke92@gmail.com>
2023-04-21 00:31:39 +0200

    Change SHOULD_RUN_FAKE_CAN from "1" to 1

--- tmux_script.sh
+++ tmux_script.sh
@@ -6,13 +6,13 @@ if [ "${SHOULD_BUILD_OPENPILOT}" == 1 ]; then
 fi
 
 tmux new -d -s default
-if [[ "${SHOULD_RUN_OPENPILOT_SIM}" == "1" ]]; then
+if [[ "${SHOULD_RUN_OPENPILOT_SIM}" == 1 ]]; then
   tmux send-keys "cd /openpilot/tools/sim && ./launch_openpilot.sh" ENTER
   tmux neww
   tmux send-keys "cd /openpilot/tools/sim && ./bridge.py $*" ENTER
   tmux neww
 fi
-if [[ "${SHOULD_RUN_FAKE_CAN}" == "1" ]]; then
+if [[ "${SHOULD_RUN_FAKE_CAN}" == 1 ]]; then
   tmux send-keys 'PYTHONPATH=/openpilot:/openpilot-bridge python -u fake_can.py' ENTER
   tmux neww
 fi
