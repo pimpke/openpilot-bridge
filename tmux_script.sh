a6c06e9cc8fa6ef86a7c8c65a51e94644a4ff47d
Dimitrije Dimic <dimke92@gmail.com>
2023-06-06 18:06:32 +0200

    Change tmux leader key inside openpilot container to C-b

--- tmux_script.sh
+++ tmux_script.sh
@@ -6,6 +6,7 @@ if [ "${SHOULD_BUILD_OPENPILOT}" == 1 ]; then
   exit 0
 fi
 
+echo 'set -g prefix C-b' >> ~/.tmux.conf
 tmux new -d -s default
 if [[ "${SHOULD_RUN_OPENPILOT_SIM}" == 1 ]]; then
   tmux send-keys "cd /openpilot/tools/sim && ./launch_openpilot.sh" ENTER
